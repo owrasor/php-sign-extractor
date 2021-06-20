e6f3bc59ef579686431761ddf1bd668639f1b791
Fabio Leal Kretzer <owrasor@gmail.com>
2021-06-19 23:00:53 -0300

    Initial commit

---
+++ src/PdfSignExtractorServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Owrasor\PdfSignExtractor;
+
+use Illuminate\Support\ServiceProvider;
+
+class PdfSignExtractorServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+    }
+
+    public function register()
+    {
+        parent::register();
+    }
+}
+
